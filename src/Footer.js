8114d9fbb72ae86bcb98d98a3b889456557bf43a
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2023-01-02 17:55:37 +0400

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import { useStore, useStoreState } from "easy-peasy";
+
+function Footer () {
+  const postCount = useStoreState((state) => state.postCount)
+
+  return ( 
+    <footer className="Footer">
+      <p>{postCount} Blog Posts</p>
+    </footer> 
+  );
+}
+
+export default Footer ;
